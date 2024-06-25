bdd39fa331755ae0421648f9d864a83f65451a52
viorelyo <viorelgurdis@gmail.com>
2024-06-26 00:38:23 +0300

    Implemented manager work-sending method

--- manager/manager.go
+++ manager/manager.go
@@ -1,24 +1,40 @@
 package manager
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 	"github.com/viorelyo/tesseract/task"
+	"github.com/viorelyo/tesseract/worker"
 )
 
 type Manager struct {
-	Pending       queue.Queue
 	Workers       []string
+	LastWorker    int
+	Pending       queue.Queue
+	TaskDb        map[uuid.UUID]*task.Task
+	EventDb       map[uuid.UUID]*task.TaskEvent
 	WorkerTaskMap map[string][]uuid.UUID
-	TaskDb        map[string][]*task.Task
-	EventDb       map[string][]*task.TaskEvent
 	TaskWorkerMap map[uuid.UUID]string
 }
 
-func (m *Manager) SelectWorker() {
-	fmt.Println("Selecting worker")
+// Round-robin naive scheduling
+func (m *Manager) SelectWorker() string {
+	var newWorker int
+	if m.LastWorker+1 < len(m.Workers) {
+		newWorker = m.LastWorker + 1
+		m.LastWorker++
+	} else {
+		newWorker = 0
+		m.LastWorker = 0
+	}
+
+	return m.Workers[newWorker]
 }
 
 func (m *Manager) UpdateTasks() {
@@ -26,5 +42,56 @@ func (m *Manager) UpdateTasks() {
 }
 
 func (m *Manager) SendWork() {
-	fmt.Println("Sending work")
+	if m.Pending.Len() > 0 {
+		w := m.SelectWorker()
+
+		e := m.Pending.Dequeue()
+		te := e.(task.TaskEvent)
+		t := te.Task
+
+		log.Printf("Pulled %v off pending queue\n", t)
+
+		m.EventDb[te.ID] = &te
+		m.WorkerTaskMap[w] = append(m.WorkerTaskMap[w], te.Task.ID)
+		m.TaskWorkerMap[t.ID] = w
+
+		t.State = task.Scheduled
+		m.TaskDb[t.ID] = &t
+
+		data, err := json.Marshal(te)
+		if err != nil {
+			log.Printf("Could not marshal task object: %v\n", t)
+		}
+
+		// todo unindent
+		url := fmt.Sprintf("http://%s/tasks", w)
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+		if err != nil {
+			log.Printf("Could not connect to [%v]: %v\n", w, err)
+			m.Pending.Enqueue(te)
+			return
+		}
+
+		d := json.NewDecoder(resp.Body)
+		if resp.StatusCode != http.StatusCreated {
+			e := worker.ErrResponse{}
+			err := d.Decode(&e)
+			if err != nil {
+				fmt.Printf("Could not decode response: %s\n", err.Error())
+				return
+			}
+			log.Printf("Response error (%d): %s", e.HTTPStatusCode, e.Message)
+			return
+		}
+
+		t = task.Task{}
+		err = d.Decode(&t)
+		if err != nil {
+			fmt.Printf("Could not decode response: %s\n", err.Error())
+			return
+		}
+		log.Printf("Task sent: %v\n", t)
+	} else {
+		log.Println("No work in the queue")
+	}
 }
